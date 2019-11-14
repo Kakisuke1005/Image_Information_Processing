97edd99a1f6bba5f8189a5ab59b9bf711f7ba2f9
Kakisuke1005 <n1005kjiam@gmai.com>
2019-11-14 15:04:37 +0900

    第五回

--- 5th/5-B-2.c
+++ 5th/5-B-2.c
@@ -10,9 +10,10 @@ void processing(void);  // 画像データをコピー
 void put_data(void);  // 画像データを出力
 void rgb_to_ybr(void);
 void ybr_to_rgb(void);
-void show_imgin_data(int num,int x,int y);  // 画像データの表示
 int round_off(double num);  // 四捨五入をする
 int round_off_cbcr(double num);  // Cb,Cr信号に対する整数化
+void show_image(int num,int key);  // 画像データの表示
+void show_image_data(int num,int x,int y,int key);  // 画像データの表示(二桁の16進数の表示))
 
 unsigned char header[HEADER_SIZE];  // ヘッダー部のデータ
 unsigned char imgin[3][IMAGE_SIZE][IMAGE_SIZE];  // データ部の画像データ
@@ -132,7 +133,7 @@ void put_data()
   FILE *fp;
   char file_name[20];
 
-  printf("\n出力ファイル名を入力して下さい:");
+  printf("出力ファイル名を入力して下さい:");
   scanf("%s",file_name);
 
   fp=fopen(file_name,"wb");
@@ -160,7 +161,6 @@ void put_data()
 
 void rgb_to_ybr(void)
 {
-  int i,j,k;
   double ycbcr[3];
   double transformation_matrix[3][3]={
     {0.2990,0.5870,0.1140},
@@ -170,32 +170,17 @@ void rgb_to_ybr(void)
 
   printf("\n＜入力信号(RGB)＞\n");
   printf("--- R ---\n");
-  for(int i=0;i<height;i++){
-    for(int j=0;j<width;j++){
-	    show_imgin_data(0,j,i);
-    }
-    printf("\n");
-  }
+  show_image(0,0);
   printf("--- G ---\n");
-  for(int i=0;i<height;i++){
-    for(int j=0;j<width;j++){
-	    show_imgin_data(1,j,i);
-    }
-    printf("\n");
-  }
+  show_image(1,0);
   printf("--- B ---\n");
-  for(int i=0;i<height;i++){
-    for(int j=0;j<width;j++){
-      show_imgin_data(2,j,i);
-    }
-    printf("\n");
-  }
+  show_image(2,0);
   printf("\n");
 
-  // 実数値としてYCbCr信号に変換
-  for(i=0;i<height;i++){
-    for(j=0;j<width;j++){
-      for(k=0;k<3;k++){
+  // YCbCr信号に変換
+  for(int i=0;i<height;i++){
+    for(int j=0;j<width;j++){
+      for(int k=0;k<3;k++){
         ycbcr[k]=transformation_matrix[k][0]*imgin[0][i][j]+transformation_matrix[k][1]*imgin[1][i][j]+transformation_matrix[k][2]*imgin[2][i][j];
       }
       ycbcr[0]=round_off(ycbcr[0]);
@@ -209,45 +194,85 @@ void rgb_to_ybr(void)
   
   printf("＜入力信号(YCbCr)＞\n");
   printf("--- Y ---\n");
-  for(int i=0;i<height;i++){
-    for(int j=0;j<width;j++){
-	    show_imgin_data(0,j,i);
-    }
-    printf("\n");
-  }
+  show_image(0,0);
   printf("--- Cb ---\n");
-  for(int i=0;i<height;i++){
-    for(int j=0;j<width;j++){
-	    show_imgin_data(1,j,i);
-    }
-    printf("\n");
-  }
+  show_image(1,0);
   printf("--- Cr ---\n");
-  for(int i=0;i<height;i++){
-    for(int j=0;j<width;j++){
-      show_imgin_data(2,j,i);
-    }
-    printf("\n");
-  }
+  show_image(2,0);
   printf("\n");
   
 }
 
 void ybr_to_rgb(void)
 {
+  double rgb[3];
   double transformation_matrix[3][3]={
     {1.0000,0.0000,1.4020},
     {1.0000,-0.3441,-0.7141},
     {1.0000,1.7720,0.0000}
   };
+
+  printf("\n＜出力信号(YCbCr)＞\n");
+  printf("--- Y ---\n");
+  show_image(0,1);
+  printf("--- Cb ---\n");
+  show_image(1,1);
+  printf("--- Cr ---\n");
+  show_image(2,1);
+  printf("\n");  
+
+  // RGB信号に変換
+  for(int i=0;i<height;i++){
+    for(int j=0;j<width;j++){
+      for(int k=0;k<3;k++){
+        rgb[k]=transformation_matrix[k][0]*imgout[0][i][j]+transformation_matrix[k][1]*(imgout[1][i][j]-128)+transformation_matrix[k][2]*(imgout[2][i][j]-128);
+      }
+      rgb[0]=round_off(rgb[0]);
+      rgb[1]=round_off(rgb[1]);
+      rgb[2]=round_off(rgb[2]);
+      imgout[0][i][j]=rgb[0];
+      imgout[1][i][j]=rgb[1];
+      imgout[2][i][j]=rgb[2];
+    }
+  }
+
+  printf("＜出力信号(RGB)＞\n");
+  printf("--- R ---\n");
+  show_image(0,1);
+  printf("--- G ---\n");
+  show_image(1,1);
+  printf("--- B ---\n");
+  show_image(2,1);
+  printf("\n");    
 }
 
-void show_imgin_data(int num,int x,int y)
+void show_image(int num,int key)
 {
-  if(imgin[num][x][y]>=16){
-    printf("%X ",imgin[num][x][y]);
-  }else{
-    printf("0%X ",imgin[num][x][y]);
+  for(int i=0;i<height;i++){
+    for(int j=0;j<width;j++){
+	    show_image_data(num,j,i,key);
+    }
+    printf("\n");
+  }
+}
+
+void show_image_data(int num,int x,int y,int key)
+{
+  switch(key){
+    case 0:
+      if(imgin[num][x][y]>=16){
+        printf("%X ",imgin[num][x][y]);
+      }else{
+        printf("0%X ",imgin[num][x][y]);
+      }
+    break;
+    case 1:
+      if(imgout[num][x][y]>=16){
+        printf("%X ",imgout[num][x][y]);
+      }else{
+        printf("0%X ",imgout[num][x][y]);
+      }
+    break;
   }
 }
 
